d2b8479c53d6bbd92bf0bf90ee1fbf2f1b39cf80
Akshat Gupta <149595957+Akshat-cyber-cloud@users.noreply.github.com>
2025-11-13 00:18:29 +0530

    Update

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 
-  const reviews = document.querySelectorAll(".review");
-  let current = 0;
+const reviews = document.querySelectorAll(".review");
+let current = 0;
 
-  function showNextReview() {
-    reviews[current].classList.remove("active");
-    current = (current + 1) % reviews.length;
-    reviews[current].classList.add("active");
+function showNextReview() {
+  reviews[current].classList.remove("active");
+  current = (current + 1) % reviews.length;
+  reviews[current].classList.add("active");
+}
+
+setInterval(showNextReview, 2000);
+
+
+const signInLink = document.getElementById('signInLink');
+const modal = document.getElementById('loginBox');
+const closeBtn = document.querySelector('.close');
+
+signInLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
   }
+});
 
-  setInterval(showNextReview, 2000); 
+
 
